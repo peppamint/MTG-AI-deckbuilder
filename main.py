6487c95d428224f92fdc24dc5746471020488a19
Chloe <chloezhong59@gmail.com>
2025-11-30 16:29:28 -0800

    added more robust input feature

--- main.py
+++ main.py
@@ -11,10 +11,13 @@ def main():
     raw_colors = input("Enter colors (comma separated letters, e.g. R,G,U):")
     colors = [c.strip() for c in raw_colors.split(",")]
 
-    
+    archetype = input("Enter corresponding number: 1 for aggro, 2 for midrange, 3 for control")
 
-    archetype = "aggro"
-    budget = False
+    budget = input("Enter budget in USD")
+
+    # colors = ['R', 'G']
+    # archetype = "aggro"
+    # budget = False
 
     deck = build_deck(colors, archetype, budget)
     print(format_deck(deck))
